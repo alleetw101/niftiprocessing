a3af73b2fe28b554c3568d9c327b7cd00743bd08
Alan Lee <alleetw101@gmail.com>
2021-02-25 19:11:56 -0500

    Updates to Nifti class and added loading and information

--- niftiprocessing/__init__.py
+++ niftiprocessing/__init__.py
@@ -1,19 +1,35 @@
 import argparse
+import os
+
+from .nifti import Nifti
 
 
 def parsercli():
-    parser = argparse.ArgumentParser(prog='ExamplePackage')
+    parser = argparse.ArgumentParser(prog='niftiprocessing')
 
-    parser.add_argument('integer', type=int, help='Integer to square')
-    parser.add_argument('--power', '-p', type=int, help='Integer to nth power')
+    # Input
+    parser.add_argument('path', type=str, help='Path to file or directory')
+
+    # Input options
+    parser.add_argument('--precision', type=int, choices=[32, 64], default=32)
+
+    # Information
+    parser.add_argument('-info', '-i', action='store_true')
+
+    # Tools
+    parser.add_argument('--denoise', '-d', action='store_true')
 
     args = parser.parse_args()
 
-    if args.power:
-        print(args.integer**args.power)
-    else:
-        print(args.integer**2)
+    # Initialize Nifti
+    niftiobject = Nifti(args.path, args.precision)
 
+    # Test arguments
+    print(args.path)
+    print(args.precision)
+
+    if args.info:
+        niftiobject.information()
 
 
 def main():
