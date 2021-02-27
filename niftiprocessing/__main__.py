408cbaffe9233f4351c110b236085d944035456e
Alan Lee <alleetw101@gmail.com>
2021-02-26 20:05:21 -0500

    Added debugging tools

--- niftiprocessing/__main__.py
+++ niftiprocessing/__main__.py
@@ -1,4 +1,12 @@
 import niftiprocessing
+import sys
+import os
 
+# Debugging
 if __name__ == '__main__':
+    test_filepath = os.path.normpath(os.getcwd()).split(os.sep)
+    test_filepath = os.path.join(os.sep, *test_filepath[:-1], 'test_directory/003434-1-1.nii.gz')
+
+    sys.argv = ['niftiprco', test_filepath]
+
     niftiprocessing.main()
