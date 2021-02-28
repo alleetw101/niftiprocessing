5b49611c38fc03d36e72f833d55542193e22b84b
Alan Lee <alleetw101@gmail.com>
2021-02-27 19:51:06 -0500

    Added --plot, --rotate, and --output

--- niftiprocessing/__init__.py
+++ niftiprocessing/__init__.py
@@ -14,7 +14,8 @@ def parsercli():
     parser.add_argument('--precision', type=int, choices=[32, 64], default=32)
 
     # Information
-    parser.add_argument('-info', '-i', action='store_true')
+    parser.add_argument('--info', '-i', action='store_true')
+    parser.add_argument('--plot', '-p', type=int)
 
     # Tools
     parser.add_argument('--denoise', '-d', action='store_true')
@@ -22,6 +23,10 @@ def parsercli():
     parser.add_argument('--normalize', '-n', action='store_true')
     parser.add_argument('--normalize-lower', type=checkintfloat, default=0.0)
     parser.add_argument('--normalize-upper', type=checkintfloat, default=1.0)
+
+    parser.add_argument('--rotate', '-r', action='store_true')
+
+    parser.add_argument('--output', '-o', action='store_true')
 
     args = parser.parse_args()
 
@@ -34,10 +39,19 @@ def parsercli():
     if args.normalize:
         niftiobject.normalize_nifti(args.normalize_lower, args.normalize_upper)
 
+    if args.rotate:
+        niftiobject.rotate_nifti()
+
+    if args.plot:
+        niftiobject.plot(args.plot)
+
+    if args.output:
+        niftiobject.write_nifti()
+
 
 def checkintfloat(x):
     if not (type(x) == float or type(x) == int):
-        TypeError('Input for --normalize must be an int or float')
+        TypeError('Input(s) for --normalize must be an int or float')
 
     return float(x)
 
