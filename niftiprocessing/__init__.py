ccba0dada56e15a3e74c478ba42b912ddd941c22
Alan Lee <alleetw101@gmail.com>
2021-03-04 08:03:14 -0500

    Added help options

--- niftiprocessing/__init__.py
+++ niftiprocessing/__init__.py
@@ -8,25 +8,25 @@ def parsercli():
     parser = argparse.ArgumentParser(prog='niftiprocessing')
 
     # Input
-    parser.add_argument('path', type=str, help='Path to file or directory')
+    parser.add_argument('path', type=str, help='Path to file')
 
     # Input options
-    parser.add_argument('--precision', type=int, choices=[32, 64], default=32)
+    parser.add_argument('--precision', type=int, choices=[32, 64], default=32, help='Sets datatype precision in float32 or 64')
 
     # Information
-    parser.add_argument('--info', '-i', action='store_true')
-    parser.add_argument('--plot', '-p', type=int)
+    parser.add_argument('--info', '-i', action='store_true', help='Outputs .nii file metadata')
+    parser.add_argument('--plot', '-p', type=int, help='Plots image of 2nd/3rd dimension in .nii file (Requires MatplotLib)')
 
     # Tools
-    parser.add_argument('--denoise', '-d', action='store_true')
+    parser.add_argument('--align', '-a', action='store_true', help='Orients .nii file in MNI152 space')
 
-    parser.add_argument('--normalize', '-n', action='store_true')
-    parser.add_argument('--normalize-lower', type=checkintfloat, default=0.0)
-    parser.add_argument('--normalize-upper', type=checkintfloat, default=1.0)
+    parser.add_argument('--normalize', '-n', action='store_true', help='Normalizes .nii file values within set values (Default: 0.,1.)')
+    parser.add_argument('--normalize-lower', type=checkintfloat, default=0.0, help='Lower value for normalization (Default: 0.)')
+    parser.add_argument('--normalize-upper', type=checkintfloat, default=1.0, help='Lower value for normalization (Default: 0.)')
 
-    parser.add_argument('--rotate', '-r', action='store_true')
+    parser.add_argument('--rotate', '-r', action='store_true', help='Rotates .nii file by 90 degress in 1st and 2nd dimension')
 
-    parser.add_argument('--output', '-o', action='store_true')
+    parser.add_argument('--output', '-o', action='store_true', help='Filename for .nii file generation (Note: Currently erases orginal metadata)')
 
     args = parser.parse_args()
 
@@ -35,6 +35,9 @@ def parsercli():
 
     if args.info:
         niftiobject.information()
+
+    if args.align:
+        niftiobject.align_nifti()
 
     if args.normalize:
         niftiobject.normalize_nifti(args.normalize_lower, args.normalize_upper)
