82881130a2e6f9f329a115079b05665e2d75d2f3
Alan Lee <alleetw101@gmail.com>
2021-03-07 15:16:11 -0500

    Added docstrings

--- niftiprocessing/numpyhelper.py
+++ niftiprocessing/numpyhelper.py
@@ -2,6 +2,17 @@ import numpy as np
 
 
 def normalize(array: np.array, lower: float, upper: float) -> np.array:
+    """Normalizes array
+
+    Normalizes input array into values between two values. Values are set in arguments as lower and upper.
+
+    Args:
+        array: A numpy array containing data points for normalization.
+        lower: An int for the lower bound of normalization.
+        upper: An int for the upper bound of normalization.
+    Returns:
+        A numpy array with normalized values between lower and upper.
+    """
     newarray = array
 
     newarray += (0 - np.min(newarray))
@@ -12,6 +23,18 @@ def normalize(array: np.array, lower: float, upper: float) -> np.array:
 
 
 def denoise(array: np.array, lower=None, upper=None) -> np.array:
+    """Denoising
+
+    Removes outlier values from array. Cutoff range is specified with lower and upper as bounds. Values are
+    repalced with 0..
+
+    Args:
+        array: A numpy array containing data points for denoising.
+        lower: An int for the lower bound of removal.
+        upper: An int for the upper bound of removal.
+    Returns:
+        A numpy array with values lower than 'lower' and higher than 'upper' replaced with 0..
+    """
     newarray = array
 
     if lower:
